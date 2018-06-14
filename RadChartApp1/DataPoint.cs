127ceefcd7565c998db47fe34ec271dd149689f0
MATSUE Yusuke <jz5@hotmail.com>
2018-06-14 19:19:11 +0900

    プロジェクト ファイルを追加します。

---
+++ RadChartApp1/DataPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadChartApp1
+{
+    public class DataPoint
+    {
+        public double Value { get; set; }
+
+        public string Category { get; set; }
+    }
+}
---
+++ RadChartApp1/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace RadChartApp1
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<DataPoint> _source = new ObservableCollection<DataPoint>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            _source.Add(new DataPoint { Category = "A", Value = 10 });
+            _source.Add(new DataPoint { Category = "B", Value = 20 });
+            _source.Add(new DataPoint { Category = "C", Value = 30 });
+
+            DataContext = _source;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var currentViewId = ApplicationView.GetForCurrentView().Id;
+            await CoreApplication.CreateNewView().Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                Window.Current.Content = new Frame();
+                ((Frame)Window.Current.Content).Navigate(typeof(SecondPage));
+                Window.Current.Activate();
+                await ApplicationViewSwitcher.TryShowAsStandaloneAsync(
+                    ApplicationView.GetApplicationViewIdForWindow(Window.Current.CoreWindow),
+                    ViewSizePreference.Default,
+                    currentViewId,
+                    ViewSizePreference.Default);
+            });
+        }
+    }
+}
